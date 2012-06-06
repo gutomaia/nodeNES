58793f67cd27fb4c7bd791a5ae05df47273d35b5
Guto Maia <guto@guto.net>
2012-06-06 01:37:58 -0300

    added the ACCUMULATOR address mode on ROL and ROR instructions

--- tests/ror_test.js
+++ tests/ror_test.js
@@ -2,6 +2,11 @@ var assert = require('assert');
 
 var compiler = require('../src/compiler.js');
 
+/*
+INFO: according to http://www.masswerk.at/6502/6502_instruction_set.html
+there is no immediate address mode. That opcode is for accumulator address
+mode.
+
 exports.test_ror_imm = function(test){
     var tokens = compiler.lexical('ROR #10');
     test.equal(2 , tokens.length);
@@ -14,6 +19,21 @@ exports.test_ror_imm = function(test){
     test.deepEqual(code, [0x6a, 0x10]);
     test.done();
 };
+*/
+
+exports.test_ror_acc = function(test){
+    var tokens = compiler.lexical('ROR A');
+    test.equal(2 , tokens.length);
+    test.equal('T_INSTRUCTION', tokens[0].type);
+    test.equal('T_ACCUMULATOR', tokens[1].type);
+    var ast = compiler.syntax(tokens);
+    test.equal(1 , ast.length);
+    test.equal('S_ACCUMULATOR', ast[0].type);
+    var code = compiler.semantic(ast);
+    test.deepEqual(code, [0x6a]);
+    test.done();
+};
+
 
 exports.test_ror_zp = function(test){
     var tokens = compiler.lexical('ROR $00');
