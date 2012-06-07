5dcdf3d8bc233c9d95044000b70ce99544d7f983
Guto Maia <guto@guto.net>
2012-06-07 15:49:17 -0300

    refactoring on the sprite

---
+++ src/sprite.js
@@ -0,0 +1,30 @@
+(function(exports){
+
+
+exports.get_sprite = function(channelA, channelB){
+    var sprite = [];
+
+    for (var y=0; y <8; y++){
+        var a = channelA[y];
+        var b = channelB[y];
+        var line = [];
+        for (var x=0; x <8; x++){
+            var bit = Math.pow(2,7-x);
+            var pixel = -1;
+            if (!(a & bit) && !(b & bit)){
+                pixel = 0;
+            } else if ((a & bit) && !(b & bit)){
+                pixel = 1;
+            } else if (!(a & bit) && (b & bit)){
+                pixel = 2;
+            } else if ((a & bit) && (b & bit)){
+                pixel = 3;
+            }
+            line.push(pixel);
+        }
+        sprite.push(line);
+    }
+    return sprite;
+};
+
+})(typeof exports === 'undefined'? this['sprite']={}: exports);
--- tests/sprite_test.js
+++ tests/sprite_test.js
@@ -1,10 +1,6 @@
 var assert = require('assert');
 var fs = require('fs');
-var Buffer = require('buffer').Buffer;
-
-var sys = require('util');
-
-var compiler = require('../src/sprite.js');
+var sprite = require('../src/sprite.js');
 
 exports.test_first_sprite = function(test){
     var chr = fs.readFileSync(__dirname + '/../static/example/scrolling/mario.chr', 'binary');
@@ -15,28 +11,7 @@ exports.test_first_sprite = function(test){
     var channelA = bin.slice(0, 8);
     var channelB = bin.slice(8, 16);
 
-    var sprite = [];
-
-    for (var y=0; y <8; y++){
-        var a = channelA[y];
-        var b = channelB[y];
-        var line = [];
-        for (var x=0; x <8; x++){
-            var bit = Math.pow(2,7-x);
-            var pixel = -1;
-            if (!(a & bit) && !(b & bit)){
-                pixel = 0;
-            } else if ((a & bit) && !(b & bit)){
-                pixel = 1;
-            } else if (!(a & bit) && (b & bit)){
-                pixel = 2;
-            } else if ((a & bit) && (b & bit)){
-                pixel = 3;
-            }
-            line.push(pixel);
-        }
-        sprite.push(line);
-    }
+    var s1 = sprite.get_sprite(channelA, channelB);
 
     var mario =
     [
@@ -50,6 +25,6 @@ exports.test_first_sprite = function(test){
         [0,3,3,2,2,3,3,2]
     ];
 
-    test.deepEqual(mario, sprite);
+    test.deepEqual(mario, s1);
     test.done();
 };
