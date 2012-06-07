a5b1b7bb1358f4c68e39be7cfe4c928890d8d649
Guto Maia <guto@guto.net>
2012-06-07 15:57:08 -0300

    just to make sure, testing the second sprite

--- tests/sprite_test.js
+++ tests/sprite_test.js
@@ -28,3 +28,30 @@ exports.test_first_sprite = function(test){
     test.deepEqual(mario, s1);
     test.done();
 };
+
+exports.test_second_sprite = function(test){
+    var chr = fs.readFileSync(__dirname + '/../static/example/scrolling/mario.chr', 'binary');
+    var bin = [];
+    for (var i = 0; i < chr.length ; i++){
+        bin.push(chr.charCodeAt(i) & 0xFF);
+    }
+    var channelA = bin.slice(16, 24);
+    var channelB = bin.slice(24, 32);
+
+    var s1 = sprite.get_sprite(channelA, channelB);
+
+    var mario =
+    [
+        [1,1,1,0,0,0,0,0],
+        [1,1,2,0,0,0,0,0],
+        [1,2,2,0,0,0,0,0],
+        [1,1,1,1,1,1,0,0],
+        [3,2,2,2,0,0,0,0],
+        [3,3,2,2,2,2,0,0],
+        [2,2,2,2,2,2,2,0],
+        [2,2,3,2,2,2,2,0]
+    ];
+
+    test.deepEqual(mario, s1);
+    test.done();
+};
