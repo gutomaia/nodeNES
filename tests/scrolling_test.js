61e100dd1a508b50a8cd54a22b2824a82259391d
Guto Maia <guto@guto.net>
2012-06-07 13:04:40 -0300

    adding more fixtures for scrolling test

--- tests/scrolling_test.js
+++ tests/scrolling_test.js
@@ -26,14 +26,65 @@ exports.test_asm_compiler_scrolling_5 = function(test){
     var tokens = compiler.lexical(code);
     var ast = compiler.syntax(tokens);
     var opcodes = compiler.semantic(ast, true);
-    /*
-    fs.open(__dirname + '/../fixtures/scrolling/test_scrolling.nes', 'w', function(status, fd) {
-        var buffer = new Buffer(opcodes);
-        fs.writeSync(fd, buffer, 0, opcodes.length, 0);
-    });
-    */
     var data = String.fromCharCode.apply(undefined, opcodes);
     var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling5.nes', 'binary');
+
+    test.equal(expected, data);
+
+    test.done();
+};
+
+
+exports.test_asm_compiler_scrolling_1 = function(test){
+    compiler.path = '/../static/example/scrolling/';
+    var code = fs.readFileSync(__dirname + '/../static/example/scrolling/scrolling1.asm', 'utf8');
+    var tokens = compiler.lexical(code);
+    var ast = compiler.syntax(tokens);
+    var opcodes = compiler.semantic(ast, true);
+    var data = String.fromCharCode.apply(undefined, opcodes);
+    var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling1.nes', 'binary');
+
+    test.equal(expected, data);
+
+    test.done();
+};
+
+exports.test_asm_compiler_scrolling_2 = function(test){
+    compiler.path = '/../static/example/scrolling/';
+    var code = fs.readFileSync(__dirname + '/../static/example/scrolling/scrolling2.asm', 'utf8');
+    var tokens = compiler.lexical(code);
+    var ast = compiler.syntax(tokens);
+    var opcodes = compiler.semantic(ast, true);
+    var data = String.fromCharCode.apply(undefined, opcodes);
+    var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling2.nes', 'binary');
+
+    test.equal(expected, data);
+
+    test.done();
+};
+
+exports.test_asm_compiler_scrolling_3 = function(test){
+    compiler.path = '/../static/example/scrolling/';
+    var code = fs.readFileSync(__dirname + '/../static/example/scrolling/scrolling3.asm', 'utf8');
+    var tokens = compiler.lexical(code);
+    var ast = compiler.syntax(tokens);
+    var opcodes = compiler.semantic(ast, true);
+    var data = String.fromCharCode.apply(undefined, opcodes);
+    var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling3.nes', 'binary');
+
+    test.equal(expected, data);
+
+    test.done();
+};
+
+exports.test_asm_compiler_scrolling_4 = function(test){
+    compiler.path = '/../static/example/scrolling/';
+    var code = fs.readFileSync(__dirname + '/../static/example/scrolling/scrolling4.asm', 'utf8');
+    var tokens = compiler.lexical(code);
+    var ast = compiler.syntax(tokens);
+    var opcodes = compiler.semantic(ast, true);
+    var data = String.fromCharCode.apply(undefined, opcodes);
+    var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling4.nes', 'binary');
 
     test.equal(expected, data);
 
