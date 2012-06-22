9691393f618b51f5b6b44ea98d4efeeedc5ae751
Guto Maia <guto@guto.net>
2012-06-22 14:38:42 -0300

    reordening the fixtures tests on scrolling_test

--- tests/scrolling_test.js
+++ tests/scrolling_test.js
@@ -7,7 +7,6 @@ var sys = require('util');
 var compiler = require('../src/compiler.js');
 
 
-
 exports.test_get_labels = function(test){
     var code = fs.readFileSync(__dirname + '/../static/example/scrolling/scrolling5.asm', 'utf8');
     var tokens = compiler.lexical(code);
@@ -19,21 +18,6 @@ exports.test_get_labels = function(test){
     test.equal(0xf030, labels.attribData);
     test.done();
 };
-
-exports.test_asm_compiler_scrolling_5 = function(test){
-    compiler.path = '/../static/example/scrolling/';
-    var code = fs.readFileSync(__dirname + '/../static/example/scrolling/scrolling5.asm', 'utf8');
-    var tokens = compiler.lexical(code);
-    var ast = compiler.syntax(tokens);
-    var opcodes = compiler.semantic(ast, true);
-    var data = String.fromCharCode.apply(undefined, opcodes);
-    var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling5.nes', 'binary');
-
-    test.equal(expected, data);
-
-    test.done();
-};
-
 
 exports.test_asm_compiler_scrolling_1 = function(test){
     compiler.path = '/../static/example/scrolling/';
@@ -85,6 +69,20 @@ exports.test_asm_compiler_scrolling_4 = function(test){
     var opcodes = compiler.semantic(ast, true);
     var data = String.fromCharCode.apply(undefined, opcodes);
     var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling4.nes', 'binary');
+
+    test.equal(expected, data);
+
+    test.done();
+};
+
+exports.test_asm_compiler_scrolling_5 = function(test){
+    compiler.path = '/../static/example/scrolling/';
+    var code = fs.readFileSync(__dirname + '/../static/example/scrolling/scrolling5.asm', 'utf8');
+    var tokens = compiler.lexical(code);
+    var ast = compiler.syntax(tokens);
+    var opcodes = compiler.semantic(ast, true);
+    var data = String.fromCharCode.apply(undefined, opcodes);
+    var expected = fs.readFileSync(__dirname + '/../fixtures/scrolling/scrolling5.nes', 'binary');
 
     test.equal(expected, data);
 
