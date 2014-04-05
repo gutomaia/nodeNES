70a958b8ee3d1aadee35c69565e8a530d1b7a69e
Guto Maia <guto@guto.net>
2014-04-05 16:38:50 -0300

    added another mock test with jQuery to sprites, improving testcoverage

--- tests/sprite_jquery_test.js
+++ tests/sprite_jquery_test.js
@@ -1,13 +1,24 @@
 var assert = require('assert');
-
+var fs = require('fs');
 var sinon = require('sinon');
-
 var sprite = require('../lib/sprite.js');
 
-exports.test_load_sprites_with_mocked_jquery = function(test){
+
+exports.test_open_file_with_jquery = function(test){
     var jQuery = {};
     jQuery.ajax = sinon.spy();
     var sprites = sprite.open_file_with_jquery('mario.chr', jQuery);
     test.ok(jQuery.ajax.calledOnce);
+    test.done();
+};
+
+exports.test_load_sprites_with_mocked_jquery = function(test){
+    var chr = fs.readFileSync(__dirname + '/../static/example/scrolling/mario.chr', 'binary');
+    global.jQuery = {};
+    sinon.stub(sprite, 'open_file_with_jquery');
+    sprite.open_file_with_jquery.withArgs('mario.chr').returns(chr);
+    var sprites = sprite.load_sprites('mario.chr');
+    test.ok(sprite.open_file_with_jquery.calledOnce);
+    global.jQuery = undefined;
     test.done();
 };
