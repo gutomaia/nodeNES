23e84dfa8849e3ad7986c0f7d4ae089afc5b5615
Guto Maia <guto@guto.net>
2014-04-07 23:59:27 -0300

    refactoring, extracted commum open_file logic from sprite and compiler to utils

--- tests/utils_jquery_test.js
+++ tests/utils_jquery_test.js
@@ -2,12 +2,14 @@ var assert = require('assert');
 var fs = require('fs');
 var sinon = require('sinon');
 var sprite = require('../lib/sprite.js');
+var utils = require('../lib/utils.js');
+
 
 
 exports.test_open_file_with_jquery = function(test){
     var jQuery = {};
     jQuery.ajax = sinon.spy();
-    var sprites = sprite.open_file_with_jquery('mario.chr', jQuery);
+    var sprites = utils.open_file_with_jquery('mario.chr', jQuery);
     test.ok(jQuery.ajax.calledOnce);
     test.done();
 };
@@ -15,10 +17,10 @@ exports.test_open_file_with_jquery = function(test){
 exports.test_load_sprites_with_mocked_jquery = function(test){
     var chr = fs.readFileSync(__dirname + '/../static/example/scrolling/mario.chr', 'binary');
     global.jQuery = {};
-    sinon.stub(sprite, 'open_file_with_jquery');
-    sprite.open_file_with_jquery.withArgs('mario.chr').returns(chr);
+    sinon.stub(utils, 'open_file_with_jquery');
+    utils.open_file_with_jquery.withArgs('mario.chr').returns(chr);
     var sprites = sprite.load_sprites('mario.chr');
-    test.ok(sprite.open_file_with_jquery.calledOnce);
     global.jQuery = undefined;
+    test.ok(utils.open_file_with_jquery.calledOnce);
     test.done();
 };
