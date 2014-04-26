bf3159e4f02f6e8ad3fb2432935a8bbc2ecbcafc
Guto Maia <guto@guto.net>
2014-04-26 15:04:07 -0300

    added pythonbrasil 8 example to nodeNES #26

---
+++ tests/pythonbrasil8_test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var fs = require('fs');
+
+var sys = require('util');
+
+var compiler = require('../lib/compiler.js');
+
+var utils = require('../lib/utils.js');
+
+var code = fs.readFileSync(__dirname + '/../static/example/pythonbrasil8/pythonbrasil8.asm', 'utf8');
+
+var bin = fs.readFileSync(__dirname + '/../fixtures/pythonbrasil8/pythonbrasil8.nes', 'binary');
+
+exports.test_nes_compiler = function(test){
+    utils.path = 'static/example/pythonbrasil8/';
+    var rom = compiler.nes_compiler(code);
+    test.deepEqual(bin, rom);
+    test.done();
+};
