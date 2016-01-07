6559175c379f99a162771304e153cac7f1bf2562
Guto Maia <guto@guto.net>
2016-01-06 23:34:02 -0200

    running Chrome without sandbox on travis

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Wed May 07 2014 23:36:30 GMT-0300 (BRT)
 
 module.exports = function(config) {
-  config.set({
+  var cfg = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -84,9 +84,21 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome', 'Firefox'],
 
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true
-  });
+  };
+
+  if (process.env.TRAVIS) {
+      cfg.browsers = ['Chrome_travis_ci', 'Firefox'];
+  }
+
+  config.set(cfg);
 };
